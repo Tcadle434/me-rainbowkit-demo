d3aa6a5513d84f29b6a8e45497225edc01f15a38
Thomas Cadle <thomas.cadle@magiceden.io>
2024-08-14 21:49:23 -0600

    rainbowkit ME wallet demo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,48 @@
-import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import type { AppProps } from 'next/app';
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
-import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
-} from 'wagmi/chains';
-import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { RainbowKitProvider, connectorsForWallets } from "@rainbow-me/rainbowkit";
+import { magicEdenWallet } from "@rainbow-me/rainbowkit/wallets";
+import { http, createConfig } from "wagmi";
+import { mainnet } from "wagmi/chains";
 
-const config = getDefaultConfig({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
-  ],
-  ssr: true,
+const connectors = connectorsForWallets(
+	[
+		{
+			groupName: "Recommended",
+			wallets: [magicEdenWallet],
+		},
+	],
+	{
+		appName: "My RainbowKit App",
+		projectId: "YOUR_PROJECT_ID",
+	}
+);
+
+const config = createConfig({
+	connectors,
+	chains: [mainnet],
+	ssr: true,
+	transports: {
+		[mainnet.id]: http(),
+	},
 });
 
 const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={client}>
-        <RainbowKitProvider>
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
+	return (
+		<WagmiProvider config={config}>
+			<QueryClientProvider client={client}>
+				<RainbowKitProvider>
+					<Component {...pageProps} />
+				</RainbowKitProvider>
+			</QueryClientProvider>
+		</WagmiProvider>
+	);
 }
 
 export default MyApp;
